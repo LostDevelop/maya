101231e5c85026a28722c6b04b92cd8fbdd14c02
Himanshu kashyap <Himmujky@gmail.com>
2024-12-03 20:11:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('messageBtn').addEventListener('click', function() {
+    const message = document.getElementById('surpriseMessage');
+    message.style.display = 'block';
+    message.textContent = 'Mayaa, you are the sunshine in everyone\'s life! 🌞💕';
+});
